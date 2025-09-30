d1fea73a0292a07a9f6a232d8adaf01aa748dcc3
YguhCode <137354838+YguhCode@users.noreply.github.com>
2025-09-30 20:23:45 -0300

    Add README.md

---
+++ Relógio/script.js
@@ -0,0 +1,31 @@
+const horas = document.getElementById("horas");
+const minutos = document.getElementById("minutos");
+const segundos = document.getElementById("segundos");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if ( h > 12 ) {
+        h = h - 12;
+        ampm = "PM";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    horas.innerText = h;
+    minutos.innerText = m;
+    segundos.innerText = s;
+    ampmEl.innerText = ampm;
+    setTimeout(() => {
+        updateClock();
+    }, 1000);
+}
+
+updateClock();
+
